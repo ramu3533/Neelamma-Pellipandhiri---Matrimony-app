f8b5136100c0dda16f22e9edc25f96a331b6b2bb
ramu3533 <ramuphysics551@gmail.com>
2025-08-16 17:02:55 +0530

    Create ProtectedRoute.tsx

---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const ProtectedRoute = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-rose-500"></div>
+      </div>
+    );
+  }
+
+  return user ? <Outlet /> : <Navigate to="/login" replace />;
+};
+
+export default ProtectedRoute;
